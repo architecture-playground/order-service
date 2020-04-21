6326bfdb31773c73ecd3d09688e4d2379f0083d8
Mikhail Shumenko <mykhailo.shumenko@auto1.com>
2020-04-21 21:47:40 +0300

    SPIKE fix tests

--- src/test/java/com/playground/order/OrderServiceApplicationTests.java
+++ src/test/java/com/playground/order/OrderServiceApplicationTests.java
@@ -1,9 +1,8 @@
 package com.playground.order;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@PlaygroundIntegrationTest
 class OrderServiceApplicationTests {
 
     @Test
